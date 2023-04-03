fbd2a5a2402308c7cc886329adea90431aa2f44d
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-03 01:08:39 -0400

    create ExpenseItem component

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,9 +1,16 @@
-import { FlatList, Text } from "react-native";
+import { FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
 const ExpensesList = ({ expenses }) => (
   <FlatList
     data={expenses}
-    renderItem={({ item }) => <Text>{item.description}</Text>}
+    renderItem={({ item }) => (
+      <ExpenseItem
+        description={item.description}
+        date={item.date}
+        amount={item.amount}
+      />
+    )}
     keyExtractor={(item) => item.id}
   />
 );

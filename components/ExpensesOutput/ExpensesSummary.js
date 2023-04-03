df8eeb10aea4e20df63b90ee76b4dab4fb0a64e2
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-02 23:04:23 -0400

    style ExpensesSummary & ExpensesOutput

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,4 +1,5 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { COLORS } from "../../constants/styles";
 
 const ExpensesSummary = ({ expenses, periodName }) => {
   const expensesSum = expenses.reduce(
@@ -7,11 +8,31 @@ const ExpensesSummary = ({ expenses, periodName }) => {
   );
 
   return (
-    <View>
-      <Text>{periodName}</Text>
-      <Text>${expensesSum.toFixed(2)}</Text>
+    <View style={styles.container}>
+      <Text style={styles.period}>{periodName}</Text>
+      <Text style={styles.sum}>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 };
 
 export default ExpensesSummary;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: COLORS.primary50,
+    borderRadius: 6,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  period: {
+    fontSize: 12,
+    color: COLORS.primary400,
+  },
+  sum: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: COLORS.primary500,
+  },
+});

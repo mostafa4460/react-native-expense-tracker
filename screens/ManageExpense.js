e9464b4a3a2f1734f221e90d9d15bae77edc31af
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-04 22:55:41 -0400

    add JSX and styles to ManageExpenses

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,16 +1,72 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { COLORS } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  const deleteExpenseHandler = () => {};
+
+  const cancelHandler = () => {};
+
+  const confirmHandler = () => {};
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: editedExpenseId ? "Edit Expense" : "Add Expense",
+      title: isEditing ? "Edit Expense" : "Add Expense",
     });
   }, [navigation, editedExpenseId]);
 
-  return <Text>ManageExpense Screen</Text>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        <Button mode="flat" onPress={cancelHandler} style={styles.button}>
+          Cancel
+        </Button>
+        <Button onPress={confirmHandler} style={styles.button}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={COLORS.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: COLORS.primary800,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: COLORS.primary200,
+    alignItems: "center",
+  },
+});

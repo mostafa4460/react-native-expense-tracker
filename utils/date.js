37d0aca05dbd3d9171ba59bfacfd4b721f4677c4
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-08 00:15:52 -0400

    create getDateMinusDays util function

--- utils/date.js
+++ utils/date.js
@@ -1,2 +1,12 @@
-export const getFormattedDate = (date) =>
-  `${date.getMonth() + 1}-${date.getDate() + 1}-${date.getFullYear()}`;
+export const getFormattedDate = (date) => {
+  const year = date.getFullYear();
+  let month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : "0" + month;
+  let day = date.getDate().toString();
+  day = day.length > 1 ? day : "0" + day;
+  return month + "-" + day + "-" + year;
+};
+
+export const getDateMinusDays = (date, days) => {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
+};

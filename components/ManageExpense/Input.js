006cc7e9d1a8593d68d7088e19c261180b658f4e
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-09 23:03:14 -0400

    create Input component

---
+++ components/ManageExpense/Input.js
@@ -0,0 +1,43 @@
+import { StyleSheet, TextInput } from "react-native";
+import { Text, View } from "react-native";
+import { COLORS } from "../../constants/styles";
+
+const Input = ({ label, textInputProps }) => {
+  const inputStyles = [styles.input];
+
+  if (textInputProps?.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputProps} />
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: COLORS.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: COLORS.primary100,
+    color: COLORS.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});

9e6962b060ebfc69db6e83cb1b64b61638fa6bff
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-15 13:58:19 -0400

    fix getFormattedDate util function

--- utils/date.js
+++ utils/date.js
@@ -1,10 +1,5 @@
 export const getFormattedDate = (date) => {
-  const year = date.getFullYear();
-  let month = (1 + date.getMonth()).toString();
-  month = month.length > 1 ? month : "0" + month;
-  let day = date.getDate().toString();
-  day = day.length > 1 ? day : "0" + day;
-  return month + "-" + day + "-" + year;
+  return date.toISOString().slice(0, 10);
 };
 
 export const getDateMinusDays = (date, days) => {

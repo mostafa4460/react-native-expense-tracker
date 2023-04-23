e192c27107b90dbc9753331531448966de460343
mostafa4460 <mostafa.nassr88@gmail.com>
2023-04-23 17:32:51 -0400

    create http util methods to store & fetch expenses

---
+++ utils/http.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const BASE_URL = "https://expense-tracker-c0f91-default-rtdb.firebaseio.com";
+
+export const storeExpense = async (expenseData) => {
+  const response = await axios.post(`${BASE_URL}/expenses.json`, expenseData);
+  return response.data.name;
+};
+
+export const fetchExpenses = async () => {
+  const response = await axios.get(`${BASE_URL}/expenses.json`);
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description, 
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
+};
